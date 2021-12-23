fa05dc6566919480f7b2985583c512c704c9b963
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-12-22 17:43:30 -0800

    adding a new function

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+//function declaration
 func main () {
 	fmt.Println("hello playground")
+	startWebServer()
+}
+
+func startWebServer(){
+	fmt.Println("starting over...")
+	// do importnat stuff
+	fmt.Println("started server")
 }
