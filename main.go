fa93aeaffe9b8ddea01f67eb9bb814c39438bb25
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-12-22 18:12:12 -0800

    update

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	//"errors"
 )
 
 //function declaration
 func main () {
-	fmt.Println("hello playground")
-	startWebServer()
+	port := 3000
+	// port, err := startWebServer(port)
+	_, err := startWebServer(port)
+	// fmt.Println(port, err)
+	fmt.Println(err)
 }
 
-func startWebServer(){
-	fmt.Println("starting over...")
+func startWebServer(port int) (int, error) {
+	fmt.Println("Starting over...")
+
 	// do importnat stuff
-	fmt.Println("started server")
+	fmt.Println("Started server on ", port)
+	// fmt.Println("Number of tries ", numberofRetries)
+	// return errors.New("somthing is wrong")
+	return port, nil
 }
